fb4b3b120084be87cf66929b5643b93d5acba5e6
pabpereza <pabloperezaradros@gmail.com>
2023-06-22 13:21:04 +0200

    Added SQL Inyection

---
+++ Controllers/inyection/iSQL.cs
@@ -0,0 +1,48 @@
+namespace WebApi.Controllers;
+
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.Sqlite;
+
+[ApiController]
+[Route("iusers")]
+
+public class DesSQL : ControllerBase
+{
+
+    // SQL INJECTION
+    [HttpGet("{id}")]
+    public IActionResult GetById(string id)
+    {
+
+        var connection = new SqliteConnection("Data Source=LocalDatabase.db");
+
+        connection.Open();
+
+        string query = "SELECT * FROM Users WHERE id='" + id + "'";
+        Console.WriteLine(query);
+
+        var command = connection.CreateCommand();
+        command.CommandText = query;
+
+        var reader = command.ExecuteReader(); 
+
+        string output = "";
+        while (reader.Read())
+         {
+            for (int i = 0; i < reader.FieldCount; i++)
+            {
+                output += reader.GetName(i) + ": "  + reader.GetValue(i) + "\n";
+            }
+  
+         }
+
+        connection.Close();
+
+        return Ok(output);
+
+    }
+}
+
+
+
