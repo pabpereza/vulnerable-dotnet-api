31e04f9fec8b9d395032653e819e1276bb798c06
pabpereza <pabloperezaradros@gmail.com>
2023-07-03 18:21:19 +0200

    Added clickjaking vulnerability

---
+++ Controllers/transport/Clickjacking.cs
@@ -0,0 +1,25 @@
+namespace WebApi.Controllers;
+
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+
+
+[ApiController]
+[Route("clickjaking")]
+
+public class Clickjaking : ControllerBase
+{
+
+    // SQL INJECTION
+    [HttpGet("{id}")]
+    public IActionResult GetSite(string id)
+    {
+		// Generate iframe with the site
+		string output = "<iframe src=\"https://"+ id + "\" width=\"100%\" height=\"100%\"></iframe>";
+
+		//Return in html format
+		return Content(output, "text/html");
+	}
+
+}
+
